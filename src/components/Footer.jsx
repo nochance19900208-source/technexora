41d2aeef8fc30c4b111d2cbee9fd123b1a36b08e
nochance19900208-source <nochance19900208@gmail.com>
2026-01-20 20:40:51 -0500

    address fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -61,7 +61,6 @@ export default function Footer() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
                 </div>
-                <span>Tokyo, Japan</span>
                 <span>1-14-2 Tamagawa, Setagaya City, Tokyo 158-0094</span>
               </div>
             </div>
